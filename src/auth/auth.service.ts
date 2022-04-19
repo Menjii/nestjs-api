969403c5c1c638482466e8694467d4c8c8eda86b
Bartosz <bartoszw8@gmail.com>
2022-04-19 21:45:26 +0200

    auth and bookmark modules added

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from "@nestjs/common";
+
+@Injectable({})
+export class AuthService {
+    signup() {
+        return 'signed up'
+    }
+
+    signin() {
+        return 'signed in'
+    }
+
+}
