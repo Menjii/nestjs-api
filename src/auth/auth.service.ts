759cc84c46081f452954e5e9a467b9ff783e6dcd
Bartosz <bartoszw8@gmail.com>
2022-04-23 19:23:13 +0200

    singup logic, validations, prisma migration

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,35 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
+import * as argon from 'argon2'
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 
 @Injectable()
 export class AuthService {
     constructor(private prisma: PrismaService) {}
-    signup() {
-        return 'signed up'
+    async signup(dto: AuthDto) {
+        // generate hash for password
+        const hash = await argon.hash(dto.password);
+        // save user to db
+        try {
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash,
+                },
+            });
+    
+            delete user.hash;
+            // return saved user
+            return user;
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
+                    throw new ForbiddenException('Credentials taken')
+                };
+            }
+            throw error; 
+        }
     }
 
     signin() {
