703cecfb93716818111995355ef329a912150da5
Bartosz <bartoszw8@gmail.com>
2022-04-23 18:14:12 +0200

    created prisma schema, module, service

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 
 @Controller('auth')
@@ -8,7 +8,10 @@ export class AuthController {
 
 
     @Post('signup')
-    signup() {
+    signup(@Body() dto:any) {
+        console.log({
+            dto
+        });
         return this.authService.signup()
     }
 
